a67e40a3f490d8570c5893283be0682e5495a8d5
Samira Milne <milne.samira@GMAIL.COM>
2021-11-09 14:30:46 -0600

    changed items+ responsive

---
+++ src/assets/meals.js
@@ -0,0 +1,36 @@
+import sushi from "./sushi.jpeg";
+import burger from "./burger.jpeg";
+import quiche from "./quiche.jpeg";
+import tacos from "./tacos.jpeg";
+
+const meals = [
+  {
+    id:"1",
+    description: "Japan",
+    name: "Sushi",
+    price: 20.0,
+    pic: sushi,
+  },
+  {
+    id:"2",
+    description: "USA",
+    name: "Buger",
+    price: 18.5,
+    pic: burger,
+  },
+  {
+    id:"3",
+    description: "France",
+    name: "Quiche",
+    price: 15.0,
+    pic: quiche,
+  },
+  {
+    id:"4",
+    description: "Mexico",
+    name: "Tacos",
+    price: 15.0,
+    pic: tacos,
+  },
+];
+export default meals;
